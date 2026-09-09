ba2872afc71901bdda523231dac89fa6c8656a89
Vemulapalli Nagendra <nagendrav8763@gmail.com>
2026-09-09 18:46:31 +0530

    Update Exp7(autonomous  taxi routing system).py

--- EXP/Exp7(autonomous  taxi routing system).py
+++ EXP/Exp7(autonomous  taxi routing system).py
@@ -1,14 +1,21 @@
-states = 5
-reward = [0, 1, 2, 3, 5]
-gamma = 0.9
+# Shortest-route policy using value iteration
+states=['A','B','C','D','E']
+graph={
+'A':[('B',2),('C',5)],
+'B':[('A',2),('D',2)],
+'C':[('A',5),('D',1)],
+'D':[('B',2),('C',1),('E',3)],
+'E':[]
+}
+goal='E'
+V={s:0 for s in states}
+for _ in range(100):
+    V={s:(0 if s==goal else min(c+V[n] for n,c in graph[s])) for s in states}
 
-value = [0] * states
+s='A'; path=[s]; cost=0
+while s!=goal:
+    n,c=min(graph[s],key=lambda x:x[1]+V[x[0]])
+    path.append(n); cost+=c; s=n
+print("Optimal taxi route:",path)
+print("Cost:",cost)
 
-for i in range(20):
-    for s in range(states - 2, -1, -1):
-        value[s] = reward[s] + gamma * value[s + 1]
-
-print("State Values")
-
-for i in range(states):
-    print("State", i, "=", round(value[i], 2))
